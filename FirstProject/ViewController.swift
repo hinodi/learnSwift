a2b76feef24a0bb639d6bf8ea3fcdecc049b413b
Huynh Ngoc Dinh <huynhngocdinh98@gmail.com>
2018-07-15 21:37:55 +0700

    Initial Commit

---
+++ FirstProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstProject
+//
+//  Created by Huynh Ngoc Dinh on 7/15/18.
+//  Copyright © 2018 Huynh Ngoc Dinh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
